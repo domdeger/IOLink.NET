638bf3d5aa79d1d080e045d471da839678e83ce0
Dominik Deschner <deschner@medialesson.de>
2022-12-25 20:25:01 +0100

    Adds and marks IDatatypeOrTypeRef

--- src/IODD.Structure/Structure/Datatypes/RecordItemT.cs
+++ src/IODD.Structure/Structure/Datatypes/RecordItemT.cs
@@ -1,5 +1,10 @@
 using IOLinkNET.IODD.Structure.Common;
+using IOLinkNET.IODD.Structure.Interfaces;
 
 namespace IOLinkNET.IODD.Structure.Datatypes;
 
-public record RecordItemT(byte Subindex, ushort BitOffset, TextRefT Name, TextRefT? Description, SimpleDatatypeT? Type, DatatypeRefT? Ref);
+public record RecordItemT(byte Subindex, ushort BitOffset, TextRefT Name, TextRefT? Description, SimpleDatatypeT? Type, DatatypeRefT? Ref)
+        : IDatatypeOrTypeRef
+{
+    DatatypeT? IDatatypeOrTypeRef.Type => Type;
+}
--- src/IODD.Structure/Structure/ProcessData/ProcessDataItemT.cs
+++ src/IODD.Structure/Structure/ProcessData/ProcessDataItemT.cs
@@ -1,6 +1,10 @@
 using IOLinkNET.IODD.Structure.Common;
 using IOLinkNET.IODD.Structure.Datatypes;
+using IOLinkNET.IODD.Structure.Interfaces;
 
 namespace IOLinkNET.IODD.Structure.ProcessData;
 
-public record ProcessDataItemT(DatatypeT? Datatype, DatatypeRefT? Ref, TextRefT Name, ushort BitLength);
+public record ProcessDataItemT(DatatypeT? Datatype, DatatypeRefT? Ref, TextRefT Name, ushort BitLength) : IDatatypeOrTypeRef
+{
+    public DatatypeT? Type => Type;
+}
