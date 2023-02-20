e30333969ffc310b3e8b75f57061377254f9255a
Dominik Deschner <domdeger@gmail.com>
2023-02-20 09:23:38 +0100

    Checkpoint

--- src/Conversion/IoddScalarConverter.cs
+++ src/Conversion/IoddScalarConverter.cs
@@ -1,4 +1,6 @@
 
+using System.Collections;
+using System.Numerics;
 using System.Text;
 
 using IOLinkNET.IODD.Resolution;
@@ -14,7 +16,7 @@ internal class IoddScalarConverter
             { Datatype: KindOfSimpleType.Boolean } => BitConverter.ToBoolean(data),
             { Datatype: KindOfSimpleType.Float } => BitConverter.ToHalf(data),
             { Datatype: KindOfSimpleType.UInteger } => GetUint(data),
-            { Datatype: KindOfSimpleType.Integer } => BitConverter.ToInt64(data),
+            { Datatype: KindOfSimpleType.Integer } => GetInt(data, typeDef.Length),
             ParsableStringDef s => ConvertString(s, data),
             _ => throw new NotImplementedException()
         };
@@ -28,6 +30,42 @@ internal class IoddScalarConverter
             > 4 => BitConverter.ToUInt64(PadToIfNeeded(8, data)),
             _ => throw new ArgumentOutOfRangeException(nameof(data), "Data is too long to be converted into a long")
         };
+
+    private static long GetInt(ReadOnlySpan<byte> data, ushort bitLength)
+        => data.Length switch
+        {
+            1 => data[0],
+            2 => BitConverter.ToInt16(data),
+            > 2 and <= 4 => BitConverter.ToInt32(PadToComplementaryIntIfNeeded(4, bitLength, data)),
+            > 4 => BitConverter.ToInt64(PadToComplementaryIntIfNeeded(8, bitLength, data))
+        };
+
+    private static ReadOnlySpan<byte> PadToComplementaryIntIfNeeded(byte size, ushort bitLength, ReadOnlySpan<byte> data)
+        => data switch
+        {
+            _ when data.Length == size => data,
+            _ when data.Length < size => PadToComplementaryInt(size, bitLength, data),
+            _ => throw new InvalidOperationException("Desired span width is smaller than the actual size of the input.")
+        };
+
+    private static ReadOnlySpan<byte> PadToComplementaryInt(byte size, ushort bitLength, ReadOnlySpan<byte> data)
+    {
+        var bitRepresentation = new BitArray(data.ToArray());
+        var signBit = bitRepresentation[^bitLength];
+
+        var result = new BitArray(size * 8, signBit);
+
+        for (int i = 0; i < bitLength; i++)
+        {
+            result[^(bitLength - i)] = bitRepresentation[^(bitLength - i)];
+        }
+
+        var resultBytes = new byte[size];
+        result.CopyTo(resultBytes, 0);
+
+        return resultBytes.AsSpan();
+    }
+
 
     private static ReadOnlySpan<byte> PadToIfNeeded(byte size, ReadOnlySpan<byte> data)
         => data switch
--- src/Tests/Conversion.Tests/IoddConverterTests.cs
+++ src/Tests/Conversion.Tests/IoddConverterTests.cs
@@ -1,3 +1,5 @@
+using System.Buffers.Text;
+
 using FluentAssertions;
 
 using IOLinkNET.Conversion;
@@ -16,6 +18,27 @@ public class IoddConverterTests
         var testRecord = new ParsableRecord("Test", new[] { recordItem, recordItem1 });
 
         var result = converter.Convert(testRecord, new byte[] { /*1111 1111*/ 0xff });
+        result.Should().BeOfType<IEnumerable<(string, object)>>();
+    }
+
+    [Fact]
+    public void CanConvertRecordT()
+    {
+        var data = Convert.FromBase64String("AB0AHAAdABMALgATAC4=");
+        var converter = new IoddConverter();
+
+        var testRecord = new ParsableRecord("DemoRecord", new[] {
+            new ParsableRecordItem(new ParsableSimpleDatatypeDef("TI_VAR_Device_Temp_Device_Temp", KindOfSimpleType.Integer, 16), "TI_VAR_Device_Temp_Device_Temp", 96, 1),
+            new ParsableRecordItem(new ParsableSimpleDatatypeDef("TI_VAR_Device_Temp_Minimum_Device_Temp_Since_Startup", KindOfSimpleType.Integer, 16), "TI_VAR_Device_Temp_Minimum_Device_Temp_Since_Startup", 80, 2),
+            new ParsableRecordItem(new ParsableSimpleDatatypeDef("TI_VAR_Device_Temp_Maximum_Device_Temp_Since_Startup", KindOfSimpleType.Integer, 16), "TI_VAR_Device_Temp_Maximum_Device_Temp_Since_Startup", 64, 3),
+            new ParsableRecordItem(new ParsableSimpleDatatypeDef("TI_VAR_Device_Temp_Minimum_Device_Temp_Lifetime", KindOfSimpleType.Integer, 16), "TI_VAR_Device_Temp_Minimum_Device_Temp_Lifetime", 48, 4),
+            new ParsableRecordItem(new ParsableSimpleDatatypeDef("TI_VAR_Device_Temp_Maximum_Device_Temp_Lifetime", KindOfSimpleType.Integer, 16), "TI_VAR_Device_Temp_Maximum_Device_Temp_Lifetime", 32, 5),
+            new ParsableRecordItem(new ParsableSimpleDatatypeDef("TI_VAR_Device_Temp_Minimum_Device_Temp_Since_Reset", KindOfSimpleType.Integer, 16), "TI_VAR_Device_Temp_Minimum_Device_Temp_Since_Reset", 16, 6),
+            new ParsableRecordItem(new ParsableSimpleDatatypeDef("TI_VAR_Device_Temp_Maximum_Device_Temp_Since_Reset", KindOfSimpleType.Integer, 16), "TI_VAR_Device_Temp_Maximum_Device_Temp_Since_Reset", 0, 7),
+         });
+
+        var result = converter.Convert(testRecord, data);
+
         result.Should().BeOfType<IEnumerable<(string, object)>>();
     }
 }
