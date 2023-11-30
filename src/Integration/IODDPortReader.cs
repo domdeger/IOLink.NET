9fea0273d47a5a92b9f27b1c39e2c601410dba36
Dominik Deschner <domdeger@gmail.com>
2023-11-30 11:12:43 +0100

    Checkpoint

--- src/Integration/IODDPortReader.cs
+++ src/Integration/IODDPortReader.cs
@@ -1,4 +1,5 @@
-﻿using IOLinkNET.Device.Contract;
+﻿using IOLinkNET.Conversion;
+using IOLinkNET.Device.Contract;
 using IOLinkNET.IODD.Provider;
 using IOLinkNET.IODD.Resolution;
 using IOLinkNET.IODD.Resolution.Contracts;
@@ -10,16 +11,20 @@ public class IODDPortReader
 {
     private readonly IMasterConnection _connection;
     private readonly IDeviceDefinitionProvider _deviceDefinitionProvider;
+    private readonly IIoddDataConverter _ioddDataConverter;
     private IProcessDataTypeResolver? _processDataTypeResolver;
+    private IParameterTypeResolver? _parameterTypeResolver;
     private ParsableDatatype? _pdInType;
     private ParsableDatatype? _pdOutType;
     private IODevice? _deviceDefinition;
 
-    public IODDPortReader(IMasterConnection connection, IDeviceDefinitionProvider deviceDefinitionProvider)
+    private byte? _port;
+
+    public IODDPortReader(IMasterConnection connection, IDeviceDefinitionProvider deviceDefinitionProvider, IIoddDataConverter ioddDataConverter)
     {
         _connection = connection;
         _deviceDefinitionProvider = deviceDefinitionProvider;
-
+        _ioddDataConverter = ioddDataConverter;
     }
 
     public async Task InitializeForPortAsync(byte port)
@@ -37,18 +42,39 @@ public class IODDPortReader
 
         _deviceDefinition = await _deviceDefinitionProvider.GetDeviceDefinitionAsync(portInfo.DeviceInformation.VendorId, portInfo.DeviceInformation.DeviceId, portInfo.DeviceInformation.ProductId);
         _processDataTypeResolver = new ProcessDataTypeResolver(_deviceDefinition);
+        _parameterTypeResolver = new ParameterTypeResolver(_deviceDefinition);
 
-        if (_processDataTypeResolver.HasCondition())
+        (_pdInType, _pdOutType) = await GetProcessDataTypesAsync(port, _processDataTypeResolver);
+        _port = port;
+    }
+
+    public async Task<object> ReadParameter(ushort index, byte subindex)
+    {
+        var value = await _connection.ReadIndexAsync(_port!.Value, index, subindex);
+        var paramTypeDef = _parameterTypeResolver!.GetParameter(index, subindex);
+
+        var convertedValue = _ioddDataConverter.Convert(paramTypeDef, value.Span);
+
+        return convertedValue;
+    }
+
+    private async Task<(ParsableDatatype? PdIn, ParsableDatatype? PdOut)> GetProcessDataTypesAsync(byte port, IProcessDataTypeResolver processDataTypeResolver)
+    {
+        ParsableDatatype? pdInType;
+        ParsableDatatype? pdOutType;
+        if (processDataTypeResolver.HasCondition())
         {
-            var condition = _processDataTypeResolver.ResolveCondition();
+            var condition = processDataTypeResolver.ResolveCondition();
             var conditionValue = await _connection.ReadIndexAsync(port, condition.VariableDef.Index, condition.ConditionDef.Subindex);
-            _pdInType = _processDataTypeResolver.ResolveProcessDataIn(conditionValue.Span[0]);
-            _pdOutType = _processDataTypeResolver.ResolveProcessDataOut(conditionValue.Span[0]);
+            pdInType = processDataTypeResolver.ResolveProcessDataIn(conditionValue.Span[0]);
+            pdOutType = processDataTypeResolver.ResolveProcessDataOut(conditionValue.Span[0]);
         }
         else
         {
-            _pdInType = _processDataTypeResolver.ResolveProcessDataIn();
-            _pdOutType = _processDataTypeResolver.ResolveProcessDataOut();
+            pdInType = processDataTypeResolver.ResolveProcessDataIn();
+            pdOutType = processDataTypeResolver.ResolveProcessDataOut();
         }
+
+        return (pdInType, pdOutType);
     }
 }
